31679a7457ce08d6ec8346649fc22c5c31cacf20
Cryptography-FPGA <55183564+Cryptography-FPGA@users.noreply.github.com>
2019-09-24 14:18:35 +0300

    Update GFmin1.v

--- GFmin1.v
+++ GFmin1.v
@@ -1,3 +1,14 @@
+------------------------------------------------------------------------------------------------
+--  This source is dedicated to the research paper enttled                                    --
+--  "An 8-bit Serialized Architecture of SEED Block Cipher for Constrained Devices"           --
+--  on IET Circuits, Devices & Systems journal                                                --
+--  Authors : Lampros Pyrgas, Filippos Pirpilidis and Paris Kitsos                            --
+--  Institute: University of the Peloponnese                                                  --
+--  Department: Electrical and Computer Engineering                                           --
+--                                                                                            --
+--  This source is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.  --
+------------------------------------------------------------------------------------------------
+
 module GFmin1(
 input  [7:0] x,
 input ch,
